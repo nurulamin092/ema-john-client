068d0d7ba08f3646d820edd5a4b8834694b2a0d8
nurulamin092 <mnurulamin092@gmial.com>
2022-04-11 15:06:10 +0600

    login form add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login page</h1>
+        <div className='form-container'>
+            <div>
+                <h1 className='form-title'> Login </h1>
+                <form action="">
+                    <div className='input-group'>
+                        <label htmlFor="email"> Email </label>
+                        <input type="email" name='email' />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' />
+                    </div>
+                    <input className='form-submit' type="submit" value='Login' />
+                </form>
+                <p>
+                    New to ema-john? <Link className='form-link' to="/signup">Create an account</Link>
+                </p>
+            </div>
         </div>
     );
 };
