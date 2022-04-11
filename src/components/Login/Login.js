c39af13e603d34561b38e3ab2921521937572592
nurulamin092 <mnurulamin092@gmial.com>
2022-04-12 00:48:47 +0600

    handle sign out added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Login.css';
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
     const [signInWithEmailAndPassword, user, error, loading] = useSignInWithEmailAndPassword(auth)
     const handleEmailBlur = (e) => {
         setEmail(e.target.value);
@@ -19,7 +22,7 @@ const Login = () => {
         signInWithEmailAndPassword(email, password)
     }
     if (user) {
-        navigate('/shop')
+        navigate(from, { replace: true });
     }
     return (
         <div className='form-container'>
