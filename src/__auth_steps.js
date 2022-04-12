2cb44887e52adb775229f5cf909e9b071b2379c5
nurulamin092 <mnurulamin092@gmial.com>
2022-04-12 12:43:09 +0600

    update auth state

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -8,9 +8,9 @@
  * 7. npm install --save react-firebase-hooks
  * 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
  * 9. if user is created redirect to the expected page
- * 10.    const [signInWithEmailAndPassword, user] = useSignInWithEmailAndPassword(auth) for sign in    
-
- * 
+ * 10. const [signInWithEmailAndPassword, user] = useSignInWithEmailAndPassword(auth) for sign in    
+ * 11. Create RequireAuth component ==> check user exists also track user information 
+ * 12. In route wrap Protected Component by using   RequireAuth component 
  * 
  */
 
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,57 +1,56 @@
 import React, { useState } from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [address, setAddress] = useState('');
     const [phone, setPhone] = useState('');
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const location = useLocation();
-    const [signInWithEmailAndPassword, user, error, loading] = useSignInWithEmailAndPassword(auth)
     const handleNameBlur = (e) => {
         setName(e.target.value);
     }
-    const handleEmailBlur = (e) => {
-        setEmail(e.target.value);
-    }
+
     const handleAddressBlur = (e) => {
         setAddress(e.target.value);
     }
     const handlePhoneBlur = (e) => {
         setPhone(e.target.value);
     }
-    const handleUserSignIn = (e) => {
+    const handleCreateUser = (e) => {
         e.preventDefault();
-        signInWithEmailAndPassword(email, address)
+        const shipping = { name, email, address, phone }
+        console.log(shipping);
     }
     return (
         <div className='form-container'>
             <div>
                 <h1 className='form-title'> Shipping Information </h1>
-                <form onSubmit={handleUserSignIn}>
+                <form onSubmit={handleCreateUser}>
                     <div className='input-group'>
                         <label htmlFor="name"> Name </label>
                         <input onBlur={handleNameBlur} type="text" name='name' required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="email"> Email </label>
-                        <input onBlur={handleEmailBlur} type="email" name='email' required />
+                        <input value={user?.email} readOnly type="email" name='email' required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="address">Address</label>
                         <input onBlur={handleAddressBlur} type="text" name='address' required />
                     </div>
                     <div className='input-group'>
-                        <label htmlFor="address">Phone Number</label>
+                        <label htmlFor="phone">Phone Number</label>
                         <input onBlur={handlePhoneBlur} type="text" name='phone' required />
                     </div>
-                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {/*  <p style={{ color: 'red' }}>{error?.message}</p>
                     {
                         loading && <p>Loading...</p>
-                    }
+                    } */}
                     <input className='form-submit' type="submit" value='Add Shipping' />
                 </form>
 
