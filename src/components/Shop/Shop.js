c9e98af0cd0cb3b56de716fd6fd037712d047440
nurulamin092 <mnurulamin092@gmial.com>
2022-04-28 11:43:20 +0600

    selected page size

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,6 +10,8 @@ const Shop = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
     const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10)
     useEffect(() => {
         fetch('http://localhost:5000/productCount')
             .then(res => res.json())
@@ -71,8 +73,16 @@ const Shop = () => {
                 <div className='pagination'>
                     {
                         [...Array(pageCount).keys()]
-                            .map(number => <button>{number + 1}</button>)
+                            .map(number => <button className={page === number ? 'selected' : ''} onClick={() => setPage(number)}>
+                                {number + 1}
+                            </button>)
                     }
+                    <select onChange={(e) => setSize(e.target.value)} name="" id="">
+                        <option value="5">5</option>
+                        <option value="10" selected >10</option>
+                        <option value="15">15</option>
+                        <option value="20">20 </option>
+                    </select>
                 </div>
             </div>
             <div className="cart-container">
